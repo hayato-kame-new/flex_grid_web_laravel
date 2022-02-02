025b63671c3105cbc24a5e9eadebd89503f17792
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-02-02 21:23:06 +0900

    first commit

---
+++ resources/views/flexweb/post01.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <meta charset="UTF-8" >
+    <title>フルーツデザートを求めて街歩き - FITTED</title>
+    <meta name="viewport" content="width=device-width">
+    <style></style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p>this is index page</p>
+</body>
+
+</html>
---
+++ resources/views/hello/index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title></title>
+    <style></style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p>this is index page</p>
+</body>
+
+</html>
+
