16de8af7bb7117fcf3351d1e143ebdf928f0525f
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-02-03 10:27:27 +0900

    images

--- resources/views/flexweb/post01.blade.php
+++ resources/views/flexweb/post01.blade.php
@@ -16,6 +16,14 @@ FITTED
 <article>
 <h1>フルーツデザートを求めて街歩き</h1>
 
+<time datetime="2020-97-25">
+    <i class="far fa-clock"></i>
+    2020年07月25日
+</time>
+
+<figure>
+    <img src="{{ asset('images/fruits.jpg') }}" alt="さまざまな果物が盛り付けられたフルーツプレート">
+</figure>
 <p>街歩きをしているとお腹が空きます。そんな時におすすめなのがフルーツデザートです。</p>
 
 <p>AA果物は手軽に食べることができます。熱中症予防や疲労回復などの効果もあるので、
@@ -32,7 +40,8 @@ FITTED
 ©︎FITTED
 </footer>
 
-
+{{-- <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script> --}}
+<script defer src="https://use.fontawesome.com/releases/v5.15.1/js/all.js"></script>
 </body>
 
 </html>
