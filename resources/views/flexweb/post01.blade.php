8a2a9c9ed2a1a72e508209989c7df462348d1407
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-02-04 09:52:03 +0900

    PC size

--- resources/views/flexweb/post01.blade.php
+++ resources/views/flexweb/post01.blade.php
@@ -10,43 +10,92 @@
 <body>
 
 <header>
-    <a href="index.html">FITTED</a>
-
-    <nav>
-        <ul>
-            <li><a href="index.html">トップ</a></li>
-            <li><a href="about.html">サイトについて</a></li>
-            <li><a href="contat.html">お問合わせ</a></li>
-        </ul>
-    </nav>
+    <div class="container">
+        <a href="index.html">FITTED</a>
+        <nav>
+            <ul>
+                <li><a href="index.html">トップ</a></li>
+                <li><a href="about.html">サイトについて</a></li>
+                <li><a href="contat.html">お問合わせ</a></li>
+            </ul>
+        </nav>
+    </div>
 </header>
 
-<div>
-<article>
-<h1>フルーツデザートを求めて街歩き</h1>
+<div class="contents">
+    <div class="container">
+        <article>
+            <h1>フルーツデザートを求めて街歩き</h1>
 
-<time datetime="2020-97-25">
-    <i class="far fa-clock"></i>
-    2020年07月25日
-</time>
+            <time datetime="2020-97-25">
+                <i class="far fa-clock"></i>
+                2020年07月25日
+            </time>
 
-<figure>
-    <img src="{{ asset('images/fruits.jpg') }}" alt="さまざまな果物が盛り付けられたフルーツプレート">
-</figure>
-<p>街歩きをしているとお腹が空きます。そんな時におすすめなのがフルーツデザートです。</p>
+            <figure>
+                <img src="{{ asset('images/fruits.jpg') }}" alt="さまざまな果物が盛り付けられたフルーツプレート">
+            </figure>
+            <p>街歩きをしているとお腹が空きます。そんな時におすすめなのがフルーツデザートです。</p>
 
-<p>AA果物は手軽に食べることができます。熱中症予防や疲労回復などの効果もあるので、
-    街歩きのエネルギー補給にピッタリです。</p>
+            <p>AA果物は手軽に食べることができます。熱中症予防や疲労回復などの効果もあるので、
+                街歩きのエネルギー補給にピッタリです。</p>
 
-<p>最近はふらっと立ち寄れるフルーツ専門店も増えてきました。珍しい南国フルーツのプレートや、
-    その場で作ってくれるフレッシュジュースも美味しくてやみつきになります。</p>
+            <p>最近はふらっと立ち寄れるフルーツ専門店も増えてきました。珍しい南国フルーツのプレートや、
+                その場で作ってくれるフレッシュジュースも美味しくてやみつきになります。</p>
 
-</article>
+        </article>
 
-<aside>
-    <img src="{{ asset('images/foot.svg') }}">
-</aside>
+        <div class="sub">
 
+
+            <aside class="profile">
+                <h2>PROFILE</h2>
+                <figure>
+                    <img src="{{ asset('images/foot.svg') }}">
+                </figure>
+                <p>街歩き&食べ歩きがライフスタイル。天気がいい日はあちこちに出没しています。</p>
+            </aside>
+
+            <aside class="topics">
+                <h2>TOPICS</h2>
+                <ul>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="{{ asset('images/tomato.jpg') }}" alt="" />
+                            </figure>
+                            <h3>甘いトマトとすっぱいトマト</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="{{ asset('images/cycle.jpg') }}" alt="" />
+                            </figure>
+                            <h3>自転車で行けるとこまで行ってみる</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="{{ asset('images/green.jpg') }}" alt="" />
+                            </figure>
+                            <h3>緑のワンポイント</h3>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <figure>
+                                <img src="{{ asset('images/time.jpg') }}" alt="" />
+                            </figure>
+                            <h3>タイムアタックでやる気を出してみる</h3>
+                        </a>
+                    </li>
+
+                </ul>
+            </aside>
+        </div>
+    </div>
 </div>
 
 <footer>
