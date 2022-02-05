1d8f063242db6138d0984738a337f8ff529cf495
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-02-05 16:02:14 +0900

    gridweb init

---
+++ resources/views/gridweb/post03.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>WAVE</title>
+    <meta name="viewport" content="width=device-width">
+</head>
+<body>
+
+<header>
+    WAVE
+</header>
+
+<nav>
+    <ul>
+        <li><a href="{{ route('gridweb.index') }}">トップ</a></li>
+        <li><a href="">サイトについて</a></li>
+        <li><a href="">お問合せ</a></li>
+    </ul>
+</nav>
+
+post03のファイルです
+
+</body>
+</html>
+
+
