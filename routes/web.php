f1f28665db3ce61137cbbf1f00230a188481aa66
hayato-kame-new <guishan.meisha0330@gmail.com>
2022-02-06 17:29:09 +0900

    sotech init

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\FlexWebController;
 use App\Http\Controllers\GridWebController;
+use App\Http\Controllers\SotechHtmlCssController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,7 +23,7 @@ Route::get('/', function () {
 
 Route::get('/hello', [HelloController::class, 'index']);
 
-Route::get('/flexweb/', [FlexWebController::class, 'index']);
+Route::get('/flexweb/', [FlexWebController::class, 'index'])->name('flexweb.index');
 Route::get('/flexweb/post01', [FlexWebController::class, 'post01'])->name('flexweb.post01');
 Route::get('/flexweb/about', [FlexWebController::class, 'about'])->name('flexweb.about');
 Route::get('/flexweb/contact', [FlexWebController::class, 'contact'])->name('flexweb.contact');
@@ -33,3 +34,9 @@ Route::get('/gridweb/post01', [GridWebController::class, 'post01'])->name('gridw
 Route::get('/gridweb/post02', [GridWebController::class, 'post02'])->name('gridweb.post02');
 Route::get('/gridweb/post03', [GridWebController::class, 'post03'])->name('gridweb.post03');
 Route::get('/gridweb/post04', [GridWebController::class, 'post04'])->name('gridweb.post04');
+
+
+Route::get('/sotech/', [SotechHtmlCssController::class, 'index'])->name('sotech.index');
+Route::get('/sotech/menu', [SotechHtmlCssController::class, 'menu'])->name('sotech.menu');
+Route::get('/sotech/access', [SotechHtmlCssController::class, 'access'])->name('sotech.access');
+Route::get('/sotech/contact', [SotechHtmlCssController::class, 'contact'])->name('sotech.contact');
